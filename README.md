9250558cd815bccb8a9cdfb66b5af6edaea4de02
Beyz61 <6trabzn1@gmail.com>
2024-10-14 10:56:12 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# project_snack_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/project_snack_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project_snack_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()

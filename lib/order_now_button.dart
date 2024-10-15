09f9da326f239a558eccd8eb0c0ba8ca5d2d022d
Beyz61 <6trabzn1@gmail.com>
2024-10-15 14:27:16 +0200

    Added Button

---
+++ lib/order_now_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:project_snack_app/widget/screens/screen_to_fav.dart';
+
+class OrderNowButton extends StatelessWidget {
+  const OrderNowButton({
+    super.key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const ScreenToFav ()));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            boxShadow: const [
+              BoxShadow(
+                color: Color.fromARGB(255, 248, 150, 183),
+                blurRadius: 8,
+                offset: Offset(1, 2), // Shadow position
+              ),
+            ],
+            borderRadius: BorderRadius.circular(10),
+            gradient: const LinearGradient(colors: [
+              Color.fromARGB(255, 243, 111, 219),
+              Color.fromARGB(218, 209, 152, 130)
+            ], 
+        begin: Alignment.centerLeft, 
+        end: Alignment.centerRight
+        ),
+        ),
+      
+        child: const Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 10,        // höhe
+            horizontal: 55       //button länge
+          ),
+          
+          child: Text("Order Now",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold)),
+        ),
+      ),
+    );
+  }
+}
+

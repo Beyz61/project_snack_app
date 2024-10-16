ecd34e7cebe8e8b30bd3438f1b607468574c6227
Beyz61 <6trabzn1@gmail.com>
2024-10-16 21:25:36 +0200

    Added Angies Yummy

--- lib/widget/screens/angis_yummy_container.dart
+++ lib/widget/screens/angis_yummy_container.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:project_snack_app/widget/screens/screen_to_fav.dart';
+import 'package:project_snack_app/order_now_button.dart';
 
 class AngisYummyContainer extends StatelessWidget {
   const AngisYummyContainer({
@@ -12,115 +12,121 @@ class AngisYummyContainer extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(20), // Runder Rahmen
       child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8), // Verschwommenheit
-        child: Container(
-          height: 200, // Höhe Container
-          width: 360,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.white, width: 0.1), // Weiße Umrandung
-            borderRadius: BorderRadius.circular(16
-            ), // Runder Rahmen                  
+      filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8), // Verschwommenheit
+      child: Container(
+      height: 200, // Höhe Container
+      width: 360,
+      decoration: BoxDecoration(
+      border: Border.all(color: Colors.white, width: 0.1
+      ), // Weiße Umrandung
+      borderRadius: BorderRadius.circular(16
+      ), // Runder Rahmen                  
+        ),
+      child: Positioned(
+       top: 50,
+       left: 20,
+       child: Column(
+       crossAxisAlignment: CrossAxisAlignment.start,
+       children: [
+      Padding(
+       padding: const
+      EdgeInsets.all(16),
+       child:
+      Column(
+       crossAxisAlignment: CrossAxisAlignment.start,
+       children: [
+      Row(
+       children: [
+       const Text(
+         "Angi's Yummy Burger",
+          style: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+           ),
           ),
-            child: Positioned(
-              top: 50,
-              left: 20,
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const
-                  EdgeInsets.all(16),
-                  child:
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            const Text(
-                              "Angi's Yummy Burger",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Expanded(child: Container()),
-                            const Icon(
-                              Icons.star,
-                              color: Color.fromARGB(255, 233, 30, 169),
-                              size: 20,
-                            ),
-                            const SizedBox(width: 4),
-                            const Text(
-                              "4.8",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 5),
-                        const Text(
-                          "Delish vegan Burger\nthat tastes like heaven",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 10,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        const Text(
-                          "₳ 13.99",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                 const SizedBox( height: 30),
-           GestureDetector(
-          onTap: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const ScreenToFav ()));
+        Expanded(child: 
+        Container()),
+        const Icon(
+        Icons.star,
+            color: Color.fromARGB(255, 233, 30, 169),
+            size: 20,
+           ),
+        const SizedBox(width: 4),
+        const Text(
+           "4.8",
+            style: TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+            ),
+          ),
+        ],
+       ),
+            const SizedBox(height: 5),
+            const Text(
+            "Delish vegan Burger\nthat tastes like heaven",
+              style: TextStyle(
+              color: Colors.white,
+              fontSize: 10,
+             ),
+            ),
+             const SizedBox(height: 10),
+             const Text(
+             "₳ 13.99",
+                style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+             ),
+              const SizedBox( height: 30),
+             GestureDetector(
+             onTap: () {
+             Navigator.push(context,
+             MaterialPageRoute(builder: 
+             (context) => const OrderNowButton ()
+             ),
+            );
           },
           child: Container(
             decoration: BoxDecoration(
-                boxShadow: const [
-                  BoxShadow(
-    color: Color.fromARGB(255, 248, 150, 183),
-    blurRadius: 2,
-    offset: Offset(1, 2), // Shadow position
+             boxShadow: const [
+              BoxShadow(
+              color: Color.fromARGB(255, 248, 150, 183),
+              blurRadius: 2,
+              offset: Offset(1, 2
+                   ), // Shadow position
                   ),
                 ],
-                borderRadius: BorderRadius.circular(6),
-                gradient: const LinearGradient(colors: [
-                  Color.fromARGB(255, 148, 65, 216),
-                  Color.fromARGB(218, 199, 130, 209)
-                ], 
+            borderRadius: BorderRadius.circular(6),
+            gradient: const LinearGradient(
+            colors: [
+            Color.fromARGB(255, 148, 65, 216),
+            Color.fromARGB(218, 199, 130, 209)
+            ], 
             begin: Alignment.centerLeft, 
             end: Alignment.centerRight
             ),
             ),
-          
-               child: const Padding(
+           child: const Padding(
               padding: EdgeInsets.fromLTRB(10,8,10,8),   
-                 child: Text("Add to order",
+              child: Text("Add to order",
               style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                   ),
-                  ),
-                 ),
+              color: Colors.white,
+              fontSize: 14,
                ),
-               ),
-               ],
               ),
+             ),
+           ),
+          ),
+           ],
+             ),
               ),
                ],
                ),
-                ),
+              ),
              ),
            ),
           );
-  }
-}
+         }
+        }
--- lib/widget/screens/screen_to_fav.dart
+++ lib/widget/screens/screen_to_fav.dart
@@ -1,7 +1,7 @@
-import "dart:ui";
 import "package:flutter/material.dart";
 import "package:project_snack_app/widget/screens/all_categories_widget.dart";
 import "package:project_snack_app/widget/screens/angis_yummy_container.dart";
+import "package:project_snack_app/widget/screens/balus_cup_container.dart";
 import "package:project_snack_app/widget/screens/moglis_cup_container.dart";
 
 class ScreenToFav extends StatelessWidget {
@@ -15,12 +15,12 @@ class ScreenToFav extends StatelessWidget {
           children: [
             Image.asset("assets/backgrounds/bg_mainscreen.png", 
               fit: BoxFit.cover), 
-              Padding(
-                padding: const EdgeInsets.all(15),
+              const Padding(
+                padding: EdgeInsets.all(15),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Padding(padding: EdgeInsets.fromLTRB(30,75,110,20),
+                    Padding(padding: EdgeInsets.fromLTRB(30,75,110,20),
                        child: Text("Choose Your Favorite Snack",
                       style: TextStyle( 
                       fontSize: 24,
@@ -29,15 +29,15 @@ class ScreenToFav extends StatelessWidget {
                       ),
                      ),
                     ),
-                  const SizedBox(height: 0),
-                  const AllCategoriesWidget(),
-                  const SizedBox(height: 40),
-                  const AngisYummyContainer(),   
-                  const SizedBox(height: 20),
-                  const Padding(padding: EdgeInsets.only(left: 22
+                  SizedBox(height: 0),
+                  AllCategoriesWidget(),
+                  SizedBox(height: 40),
+                  AngisYummyContainer(),   
+                  SizedBox(height: 20),
+                  Padding(padding: EdgeInsets.only(left: 22
                   ),
                   ),
-                  const Text("  We Recommend",
+                  Text("  We Recommend",
                             style: TextStyle( 
                       fontSize: 22,
                       fontWeight: FontWeight.bold,
@@ -45,111 +45,13 @@ class ScreenToFav extends StatelessWidget {
                    ),   
                 textAlign: TextAlign.start,
                             ),
-                            const SizedBox(height: 16),
+                            SizedBox(height: 16),
                            SingleChildScrollView(scrollDirection: Axis.horizontal,
                      child: Row(
                      children: [
-                      const MoglisCupContainer(),
-                      const SizedBox(width: 26),
-                     GestureDetector(
-                     onTap: () {
-                       showDialog(
-                     context: context,
-                     builder: (BuildContext context) => AlertDialog(
-                     title: const Text("Choose your Sound"),
-                     content: const Text("Sound \nSound \nSound \nSound"),
-                     actions: <Widget>[
-                      TextButton(
-                     onPressed: () {
-                      Navigator.of(context).pop(
-                    );
-                    },
-                    child: const Text(
-                      "Save"
-                     ),
-                    ),
-                  ]
-                 )
-               );
-             },
-                     child: ClipRRect(
-                     child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 1000, sigmaY: 1000),           
-                      child: Container(
-                       width: 204,
-                       height: 280,
-                    decoration: BoxDecoration(
-                     borderRadius: BorderRadius.circular(24),
-                     border: Border.all(color: Colors.white, width: 0.2),
-                     gradient: const LinearGradient(colors: [
-                     Color.fromARGB(57, 82, 82, 82),
-                     Color.fromARGB(132, 134, 90, 255),
-                     Color.fromARGB(132, 134, 90, 255),
-                     Color.fromARGB(132, 134, 90, 255),
-                     Color.fromARGB(155, 105, 28, 177)
-                     ],stops: [
-                     0.1,
-                     0.35,
-                     0.5,
-                     0.6,
-                     0.9
-                    ],
-                     begin: Alignment.topCenter, 
-                     end: Alignment.bottomCenter)
-                     ),
-                       child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                         child: Column(
-                         crossAxisAlignment: CrossAxisAlignment.start,
-                           children: [
-                            Transform.scale(
-                             scale: 1.0,
-                             child: Image.asset("assets/grafics/cat cupcakes_3D.png"),),
-                             const Text("Balu`s Cup",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 12,
-                                color: Colors.white),
-                                ),
-                                const Text("Pistacihio ice cream",
-                                style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 11,
-                                fontWeight: FontWeight.w300
-                                ),
-                                ),
-                                const SizedBox(height: 16
-                                ),
-                                const Row(
-                                 children: [
-                                  Text(
-                                     "₳8.99",
-                                      style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w400
-                                      ),
-                                    ),
-                                   Expanded(child: SizedBox()),
-                                   Icon(Icons.favorite_border_rounded,
-                                      color: Colors.white, 
-                                      size: 12
-                                      ),
-                                   Text("190",
-                                      style: TextStyle(
-                                        color: Colors.white, 
-                                        fontSize: 12
-                                        ),
-                                       ),
-                                      ],
-                                     ),
-                                    ],
-                                    ),
-                                    ),
-                                   ),
-                                 ),
-                               ),
-                             ),
+                      MoglisCupContainer(),
+                      SizedBox(width: 26),
+                     BalusCupContainer(),
                            ],
                          ),
                        ),
